efcaf575ff85ada97fc7844c1ee530e0f9ceabfc
Thomas Fillon <thomas@parisson.com>
2016-02-16 18:00:31 +0100

    Add conda build recipe

---
+++ conda.recipe/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ls
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DDL_ROOT=/usr/lib/x86_64-linux-gnu/ -DWITH_FFTW3=ON -DWITH_HDF5=ON -DWITH_LAPACK=ON -DWITH_MPG123=ON ..
+make
+make install
+
+# Add more build steps here, if they are necessary.
+#cd $PREFIX
+#mv python_packages/yaafelib ./lib/python2.7/
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
